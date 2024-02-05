3f4845c5c430e8e4134565016a77451b619abf79
Jeremy <1000jeremy@gmail.com>
2024-02-04 21:18:08 -0700

    Add a base template that creates a shared navbar

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,5 @@
+from flask import render_template
 from app import app
-
-# Define the base/index view function 
-@app.route('/')             # When the user goes to either of these routes
-@app.route('/index')        
-def index():                # This function is called and the browser it passed whatever is returned
-    return "Hello, World!"
-
 
 # @app.route is a python decorator --> these modify the functions that follow
 # them 
@@ -13,3 +7,29 @@ def index():                # This function is called and the browser it passed 
 #   --> in this case (@app.route) crates an association between the URL and the function
 #       --> When the user visits either of these urls, this function is called AND IT'S
 #           RETURN VALUE IS PASSED BACK TO THE BROWSER AS A RESPONSE
+
+# Define the base/index view function 
+@app.route('/')             # When the user goes to either of these routes
+@app.route('/index')        
+def index():                # This function is called and the browser it passed whatever is returned
+    user = {'username': 'Jeremy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful  day in Portland'
+        }, 
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'suuuuuh dude'
+        },
+        {
+            'author': {'username': 'Jones'},
+            'body': 'wazzzzzzup'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
